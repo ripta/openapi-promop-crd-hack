04c5ab34df3779f7ba8960bb278331849519e2b8
Ripta Pasay <ripta@users.noreply.github.com>
2019-08-01 01:09:35 -0700

    main: cleanup unused code

--- main.go
+++ main.go
@@ -6,10 +6,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"sync"
 
 	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
-	"github.com/go-openapi/jsonreference"
 	"github.com/go-openapi/spec"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apiserver/pkg/endpoints/openapi"
@@ -20,11 +18,6 @@ import (
 const (
 	definitionPrefix = "#/definitions/"
 )
-
-var buildDefinitions sync.Once
-var definitions map[string]common.OpenAPIDefinition
-var moswag *spec.Swagger
-var namer *openapi.DefinitionNamer
 
 func main() {
 	r, err := os.Open("swagger.json")
@@ -41,7 +34,6 @@ func main() {
 		log.Fatalf("cannot unmarshal swagger.json: %+v", err)
 	}
 
-	// obj := monitoringv1.DefaultCrdKinds.Prometheus
 	namer := openapi.NewDefinitionNamer(runtime.NewScheme())
 	cfg := &common.Config{
 		ProtocolList: []string{"https"},
@@ -51,25 +43,10 @@ func main() {
 				Version: "v1",
 			},
 		},
-		// CommonResponses: map[int]spec.Response{
-		// 	401: {
-		// 		ResponseProps: spec.ResponseProps{
-		// 			Description: "Unauthorized",
-		// 		},
-		// 	},
-		// },
-		GetDefinitions: mergedDefinitions(monitoringv1.GetOpenAPIDefinitions, vanilla.Definitions, namer),
-		// GetDefinitions:    mergedDefinitions(monitoringv1.GetOpenAPIDefinitions),
+		GetDefinitions:    mergedDefinitions(monitoringv1.GetOpenAPIDefinitions, vanilla.Definitions, namer),
 		GetDefinitionName: namer.GetDefinitionName,
-		// GetDefinitionName: func(name string) (string, spec.Extensions) {
-		// 	buildDefinitions.Do(buildDefinitionsFunc)
-		// return namer.GetDefinitionName(name)
-		// },
-		// GetOperationIDAndTags: openapi.GetOperationIDAndTags,
 	}
 
-	// var def common.OpenAPIDefinition
-	// def, _ := builder.BuildOpenAPIDefinitionsForResource(obj, cfg)
 	names := []string{
 		"github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1.Alertmanager",
 		"github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1.PodMonitor",
@@ -81,41 +58,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("cannot build openapi definitions: %+v", err)
 	}
+
 	j, err := json.Marshal(swag)
-	// defs := monitoringv1.GetOpenAPIDefinitions(jsonRef)
-	// j, err := json.Marshal(defs)
 	if err != nil {
 		log.Fatalf("cannot marshal: %+v", err)
 	}
 	fmt.Println(string(j))
-}
-
-func jsonRef(name string) spec.Ref {
-	// return spec.MustCreateRef("#/definitions/" + common.EscapeJsonPointer(name))
-	// return spec.MustCreateRef("#/definitions/" + name)
-	return spec.Ref{
-		Ref: jsonreference.MustCreateRef("#/definitions/" + name),
-	}
-}
-
-// func buildDefinitionsFunc() {
-// 	namer = openapi.NewDefinitionNamer(runtime.NewScheme())
-// 	definitions = generatedopenapi.GetOpenAPIDefinitions(func(name string) spec.Ref {
-// 		defName, _ := namer.GetDefinitionName(name)
-// 		return spec.MustCreateRef(definitionPrefix + common.EscapeJsonPointer(defName))
-// 	})
-// }
-
-func definitionGenerator(defs spec.Definitions) common.GetOpenAPIDefinitions {
-	return func(ref common.ReferenceCallback) map[string]common.OpenAPIDefinition {
-		oads := map[string]common.OpenAPIDefinition{}
-		for k, v := range defs {
-			oads[k] = common.OpenAPIDefinition{
-				Schema: v,
-			}
-		}
-		return oads
-	}
 }
 
 func mergedDefinitions(orig common.GetOpenAPIDefinitions, fallback spec.Definitions, namer *openapi.DefinitionNamer) common.GetOpenAPIDefinitions {
